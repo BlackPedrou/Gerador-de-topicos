b4c958daade33b282f73f947248469f766fc9906
Pedro Eduardo <contato.pedroedu@gmail.com>
2017-03-31 19:40:44 -0300

    Create index.php

---
+++ toppic/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<HTML>
+<HEAD><center><img src="http://i.imgur.com/QQKENBl.png"></center>
+<META http-equiv=Content-Type content="text/html; charset=windows-1252"><META content="MSHTML 6.00.5730.13" name=GENERATOR></HEAD>
+<BODY bgColor=#ececfb background=""><DIV align=center><FORM action=criar_model.php method=post>
+<BR><code><strong>Link imagem Logo:</strong></code><BR>
+<p>&nbsp;<INPUT style="WIDTH: 350px; HEIGHT: 20px" size=21 name=imag_logo required></P>
+<P><BR><code>Introdução</code><BR>
+<TEXTAREA class=paste_textarea onkeydown="return catchTab(this,event)" style="WIDTH: 400px; HEIGHT: 48px" name=codigo_criar rows=0 cols=24 required></TEXTAREA> 
+<BR><BR><code>Função</code>&nbsp; <BR>
+<TEXTAREA class=paste_textarea onkeydown="return catchTab(this,event)" style="WIDTH: 400px; HEIGHT: 48px" name=func_text rows=0 cols=24 required></TEXTAREA> 
+<BR><BR><code><STRONG>Link Pastebin Download:</STRONG></code></P>
+<P>&nbsp;<INPUT style="WIDTH: 350px; HEIGHT: 20px" size=48 name=dowmload_text required>
+</P>
+<P><BR><INPUT type=submit value="Gerar Modelo"> 
+</P></FORM></DIV></BODY>
+<center><code>Creditos: ipsBruno e KinG7</code></center>
+</HTML>
