21f1aa98fe0e6f5199c8ceb2dae731b4d38617f5
Pedro Eduardo <contato.pedroedu@gmail.com>
2017-03-31 19:41:27 -0300

    Create criar_model.php

---
+++ toppic/criar_model.php
@@ -0,0 +1,32 @@
+<?php
+
+$imag_lg = $_POST["imag_logo"];
+$introduc = $_POST["codigo_criar"];
+$func_txt  = $_POST["func_text"];
+$dowmaaload_txt = $_POST["dowmload_text"]; 
+
+$uerril = "[URL=$dowmaaload_txt]";
+
+echo "<head><center>Modelo Gerado com sucesso! <mark>Copie o Código Abaixo.</mark> <p></p></head>
+<body><textarea
+cols=150 rows=150 onfocus=this.select() onmouseover=this.focus() name=text_dkn>
+[center][IMG]$imag_lg [/IMG][/center]
+
+[SIZE=6][FONT=Courier New][COLOR=SlateGray]Introdução [/COLOR][/FONT][/SIZE]
+
+[indent][COLOR=Gray]$introduc [/COLOR][/indent]
+
+[SIZE=6][FONT=Courier New][COLOR=SlateGray]Funções [/COLOR][/FONT][/SIZE]
+
+[indent][COLOR=Gray]$func_txt [/COLOR][/indent]
+
+[SIZE=6][FONT=Courier New][COLOR=SlateGray]Download[/COLOR][/FONT][/SIZE]
+
+[indent][COLOR=Gray]
+
+$uerril [IMG]http://i.imgur.com/c3xoS4w.png[/IMG][SIZE=4]Pastebin[/SIZE][/URL][/COLOR][/indent]
+
+</textarea></center></body>
+";
+?>
+<html><BODY bgColor=#ececfb background=""><center>Volte Sempre :)</center></html>
